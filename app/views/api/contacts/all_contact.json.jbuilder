b8c818c510cf10686b937d0e55ad4e900f8a3154
Cody White <cody.white362@gmail.com>
2018-10-16 20:21:02 -0500

    REST contacts app

---
+++ app/views/api/contacts/all_contact.json.jbuilder
@@ -0,0 +1,1 @@
+json.contacts @all_contacts
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get "/contacts/:id" => "contacts#show"
+    get "/contacts" => "contacts#index"
+    post "/contacts" => "contacts#create"
+    patch "/contacts/:id" => "contacts#update"
+    delete "contacts/:id" => "contacts#destroy"
+  end
+end
